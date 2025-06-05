0b036cd5b80fb14930385a5ed2a3019e17071217
codewmanas <manas.kolaskar@somaiya.edu>
2025-06-05 21:28:52 +0530

    Setup NativeWind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Text className="text-3xl text-black  dark:text-white font-bold">Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#1E40AF",
+        accent: "#FACC15"
+      }
+    },
+  },
+  plugins: [],
+}
+
